7af46b2c903d86b563b5d9f5023e4ca4da60a2bd
Sila-Md <hackersila2@gmail.com>
2026-05-09 00:37:34 +0300

    Update savecontact.js

--- plugins/savecontact.js
+++ plugins/savecontact.js
@@ -3,33 +3,19 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions');
 
-// FakevCard sawa na zilizopita
-const fkontak = {
-    "key": {
-        "participant": '0@s.whatsapp.net',
-        "remoteJid": '0@s.whatsapp.net',
-        "fromMe": false,
-        "id": "Halo"
-    },
-    "message": {
-        "conversation": "𝚂𝙸𝙻𝙰"
-    }
-};
-
 const getContextInfo = (m) => {
     return {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363402325089913@newsletter',
-            newsletterName: '© 𝐒𝐈𝐋𝐀 𝐌𝐃',
+            newsletterJid: '120363424973782944@newsletter',
+            newsletterName: '𝐓𝐘𝐑𝐄𝐗 𝐌𝐃',
             serverMessageId: 143,
         }
     };
 };
 
-//vcf//
 cmd({
     pattern: 'savecontact',
     alias: ["vcf","scontact","savecontacts"],
@@ -39,37 +25,31 @@ cmd({
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         if (!isGroup) {
-            return await conn.sendMessage(from, { 
-                text: "❌ 𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚒𝚜 𝚏𝚘𝚛 𝚐𝚛𝚘𝚞𝚙𝚜 𝚘𝚗𝚕𝚢.\n\n> © Powered by Sila Tech", 
-                contextInfo: getContextInfo({ sender: sender })
-            }, { quoted: fkontak });
+            return reply("This command is for groups only.\n\n> ® Powered by Tyrex Tech");
         }
-        
+
         if (!isOwner) {
-            return await conn.sendMessage(from, { 
-                text: "🚫 *𝚃𝚑𝚒𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚒𝚜 𝚏𝚘𝚛 𝚝𝚑𝚎 𝚘𝚠𝚗𝚎𝚛 𝚘𝚗𝚕𝚢*\n\n> © Powered by Sila Tech", 
-                contextInfo: getContextInfo({ sender: sender })
-            }, { quoted: fkontak });
+            return reply("🚫 *This command is for the owner only*\n\n> ® Powered by Tyrex Tech");
         }
 
         let card = quoted || m;
         let cmiggc = groupMetadata;
         const { participants } = groupMetadata;
-        
+
         let orgiggc = participants.map(a => a.id);
         let vcard = '';
         let noPort = 0;
-        
+
         for (let a of cmiggc.participants) {
             vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:[${noPort++}] +${a.id.split("@")[0]}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`;
         }
 
         let nmfilect = './contacts.vcf';
-        
+
         await conn.sendMessage(from, { 
-            text: '𝚂𝚊𝚟𝚒𝚗𝚐 ' + cmiggc.participants.length + ' 𝚙𝚊𝚛𝚝𝚒𝚌𝚒𝚙𝚊𝚗𝚝𝚜 𝚌𝚘𝚗𝚝𝚊𝚌𝚝',
+            text: 'Saving ' + cmiggc.participants.length + ' participants contact',
             contextInfo: getContextInfo({ sender: sender })
-        }, { quoted: fkontak });
+        }, { quoted: mek });
 
         fs.writeFileSync(nmfilect, vcard.trim());
         await sleep(2000);
@@ -77,24 +57,15 @@ cmd({
         await conn.sendMessage(from, {
             document: fs.readFileSync(nmfilect), 
             mimetype: 'text/vcard', 
-            fileName: 'sila_md.vcf', 
-            caption: `╭━━〔 📇 *𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝚂 𝚂𝙰𝚅𝙴𝙳* 〕━━┈⊷
-┃
-┃ 𝙶𝚛𝚘𝚞𝚙 𝙽𝚊𝚖𝚎: *${cmiggc.subject}*
-┃ 𝙲𝚘𝚗𝚝𝚊𝚌𝚝𝚜: *${cmiggc.participants.length}*
-┃
-╰━━━━━━━━━━━━━━━━━━┈⊷
-> © Powered by Sila Tech`,
+            fileName: 'tyrex_md.vcf', 
+            caption: `╭┄┄┄🌸🌹 *CONTACTS SAVED* 🌹🌸┄┄┄⊷\n┃\n┃ Group Name: *${cmiggc.subject}*\n┃ Contacts: *${cmiggc.participants.length}*\n┃\n╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⊷\n> ® Powered by Tyrex Tech`,
             contextInfo: getContextInfo({ sender: sender })
-        }, { quoted: fkontak });
+        }, { quoted: mek });
 
-        fs.unlinkSync(nmfilect); // Cleanup the file after sending
-        
+        fs.unlinkSync(nmfilect);
+
     } catch (err) {
         console.error("Savecontact error:", err);
-        await conn.sendMessage(from, { 
-            text: err.toString(),
-            contextInfo: getContextInfo({ sender: sender })
-        }, { quoted: fkontak });
+        reply(err.toString());
     }
 });
